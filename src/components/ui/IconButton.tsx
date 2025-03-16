100b97593e2d7dc4cdf5d48e7f4b52e14d5caf02
farikkm <farikk.v.2@gmail.com>
2025-03-16 12:19:31 +0500

    IconButton.tsx width property was updated

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -7,7 +7,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement>{
 
 const IconButton = ({ children, className = "", ...props }: Props) => {
     return (
-        <button className={`bg-[#2C2D58] w-12 h-12 flex justify-center items-center rounded-4xl ${className}`} {...props}>
+        <button className={`bg-[#2C2D58] min-w-12 h-12 flex justify-center items-center rounded-4xl ${className}`} {...props}>
             {children}
         </button>
     )
