e919abe2ffd24c5879cb31a413ae3f6a992c8bc2
farikkm <farikk.v.2@gmail.com>
2025-03-20 11:51:24 +0500

    LanguageGuard was optimized and config.ts was created

--- src/components/app/LanguageGuard.tsx
+++ src/components/app/LanguageGuard.tsx
@@ -1,31 +1,36 @@
+import { config } from "@/config";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
 
-const supportedLanguages = ["ru", "en", "uz"];
+const { supportedLanguages } = config;
 
 function LanguageGuard({ setLangKey }: { setLangKey: (lng: string) => void; }) {
-  const { lng } = useParams();
+  const { lng } = useParams<{ lng: string }>();
   const navigate = useNavigate();
   const location = useLocation();
   const { i18n } = useTranslation()
 
+  // При вводе не существующего языка.
   useEffect(() => {
-    if (!supportedLanguages.includes(lng || "")) {
+    const currentLanguage = lng || "";
+    const page = location.pathname.replace(/^\/[^/]+\//, "");    
+
+    if (!supportedLanguages.includes(currentLanguage)) {
       navigate(
-        `/${i18n.language}${location.pathname.replace(/^\/[^/]+/, "")}`,
+        `/${i18n.language}/${page}`,
         { replace: true }
       );
     }
   }, [lng, navigate]);
 
+  // При изменении языка страницы.
   useEffect(() => {
+    const page = location.pathname.replace(/^\/[^/]+\//, "")
+
     const onLanguageChange = (newLng: string) => {
       if (supportedLanguages.includes(newLng) && newLng !== lng) {
-        const newPath = `/${newLng}${location.pathname.replace(
-          /^\/[^/]+/,
-          ""
-        )}`;
+        const newPath = `/${newLng}/${page}`;
         navigate(newPath, { replace: true });
         setLangKey(newLng); 
       }
